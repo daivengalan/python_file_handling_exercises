99ffd4077686a334449ecd91785887075c1e0a48
daivengalan <daivenclark@gmail.com>
2026-04-22 11:33:18 +0800

    restructuring the project into a class-based system. defined NumberSplitter with an init method to handle file paths. this will make the code reusable if i want to use different filenames later.

--- even_and_odd_splitter/even_and_odd_splitter.py
+++ even_and_odd_splitter/even_and_odd_splitter.py
@@ -1,10 +1,16 @@
+class number_splitter:
+    def __init__(self, input_file="numbers.txt"):
+        self.input_file = input_file
+        self.even_file = "even.txt"
+        self.odd_file = "odd.txt"
+
 input_stream = open("numbers.txt", "r")
 even_file = open("even.txt", "w")
 odd_file = open("odd.txt", "w")
 
 for line in input_stream:
     clean_line = line.strip()
-    if clean_line: # skip empty lines
+    if clean_line:
         current_number = int(clean_line)
         if current_number % 2 == 0:
             even_file.write(str(current_number) + "\n")
