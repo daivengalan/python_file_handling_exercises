0b22b90ad0f80cf7568b3472e5aa04c0bd1cdd36
daivengalan <daivenclark@gmail.com>
2026-04-22 10:59:33 +0800

    fixed a crash that happens when there are empty lines in the input file, i added an if-line.strip()

--- even_and_odd_splitter/even_and_odd_splitter.py
+++ even_and_odd_splitter/even_and_odd_splitter.py
@@ -3,11 +3,13 @@ even_file = open("even.txt", "w")
 odd_file = open("odd.txt", "w")
 
 for line in input_stream:
-    current_number = int(line.strip())
-    if current_number % 2 == 0:
-        even_file.write(str(current_number) + "\n")
-    else:
-        odd_file.write(str(current_number) + "\n")
+    clean_line = line.strip()
+    if clean_line: # skip empty lines
+        current_number = int(clean_line)
+        if current_number % 2 == 0:
+            even_file.write(str(current_number) + "\n")
+        else:
+            odd_file.write(str(current_number) + "\n")
 
 input_stream.close()
 even_file.close()
