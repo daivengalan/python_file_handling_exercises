02d7229949dc03bbfd02f529fa8046dcfbd01ac8
daivengalan <daivenclark@gmail.com>
2026-04-22 10:52:28 +0800

    now writing the results to separate text files instead of just printing them

--- even_and_odd_splitter/even_and_odd_splitter.py
+++ even_and_odd_splitter/even_and_odd_splitter.py
@@ -1,8 +1,14 @@
 input_stream = open("numbers.txt", "r")
+even_file = open("even.txt", "w")
+odd_file = open("odd.txt", "w")
+
 for line in input_stream:
     current_number = int(line.strip())
     if current_number % 2 == 0:
-        print(f"{current_number} is even")
+        even_file.write(str(current_number) + "\n")
     else:
-        print(f"{current_number} is odd")
+        odd_file.write(str(current_number) + "\n")
+
 input_stream.close()
+even_file.close()
+odd_file.close()
