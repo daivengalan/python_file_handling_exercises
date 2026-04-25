199ec00f77b0100bfa4805d7edd32d0b7f4f8f08
daivengalan <daivenclark@gmail.com>
2026-04-25 13:03:09 +0800

    added the logic to find the highest grade but instead of Jose Rizal with a 1.00 that shows up, Diego Silang with 2.50 shows up. i need to fix the logic.

--- highest_gwa_finder/highest_gwa_finder.py
+++ highest_gwa_finder/highest_gwa_finder.py
@@ -3,6 +3,24 @@ class Student:
         self.student_name = student_name
         self.gwa_score = float(gwa_score)
 
-# testing if the class works
-test_student = Student("Test User", "1.75")
-print(test_student.student_name, test_student.gwa_score)
+class GwaProcessor:
+    def __init__(self, file_source):
+        self.file_source = file_source
+        self.students = []
+
+    def load_data(self):
+        with open(self.file_source, "r") as file:
+            for line in file:
+                name, gwa = line.strip().split(",")
+                self.students.append(Student(name, gwa))
+# (Keep the classes from Commit 6, then add this method inside GwaProcessor)
+    def get_highest(self):
+        # MISTAKE: Using max() assumes a higher number is better
+        highest = max(self.students, key=lambda s: s.gwa_score)
+        return highest
+
+# (Add at the bottom)
+processor = GwaProcessor("students.txt")
+processor.load_data()
+top = processor.get_highest()
+print(f"Highest goes to {top.student_name} with {top.gwa_score}")
