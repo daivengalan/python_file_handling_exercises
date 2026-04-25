a9cbfcbe2cc230e28083457786525d1588ce9322
daivengalan <daivenclark@gmail.com>
2026-04-25 13:54:24 +0800

    added a basic while loop to keep asking for lines. it doesn't save yet, just testing the loop.

--- my_life_diary/my_life_diary.py
+++ my_life_diary/my_life_diary.py
@@ -2,4 +2,12 @@ class DiaryWriter:
     def __init__(self, filename):
         self.filename = filename
 
-my_diary = DiaryWriter("mylife.txt")
+    def collect_lines(self):
+        while True:
+            line = input("Enter line: ")
+            more = input("Are there more lines y/n? ")
+            if more.lower() == 'n':
+                break
+
+writer = DiaryWriter("mylife.txt")
+writer.collect_lines()
