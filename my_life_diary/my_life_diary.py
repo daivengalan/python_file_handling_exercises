928dbaa1d1e672274080731954c8d81907223778
daivengalan <daivenclark@gmail.com>
2026-04-25 14:13:26 +0800

    added \n so each entry actually goes on a new line. much better. also added a try-except block just in case there's a permission error or something when creating the file.

--- my_life_diary/my_life_diary.py
+++ my_life_diary/my_life_diary.py
@@ -7,11 +7,24 @@ class DiaryWriter:
             user_input = input("Enter line: ")
             # MISTAKE: Opening file inside the loop with 'w' overwrites everything
             with open(self.filename, "a") as file:
-                file.write(user_input)
+                file.write(user_input + "\n ")
 
             choice = input("Are there more lines y/n? ")
             if choice.lower() == 'no':
                 break
+
+    def write_to_diary(self):
+        try:
+            while True:
+                user_text = input("Enter line: ")
+                with open(self.filename, "a") as file:
+                    file.write(user_text + "\n")
+
+                choice = input("Are there more lines y/n? ")
+                if choice.lower() == 'n':
+                    break
+        except Exception as error:
+            print(f"Something went wrong: {error}")
 
 writer = DiaryWriter("mylife.txt")
 writer.collect_lines()
