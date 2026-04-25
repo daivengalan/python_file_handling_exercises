8a24af4c139c3f5efe73b8d7ef4aa5b9b5c0556b
daivengalan <daivenclark@gmail.com>
2026-04-25 12:49:21 +0800

    fixed the spacing issue using strip(). also added partition, names and gwa labels so it's much cleaner now.

--- highest_gwa_finder/highest_gwa_finder.py
+++ highest_gwa_finder/highest_gwa_finder.py
@@ -2,5 +2,7 @@ file_path = "students.txt"
 
 with open(file_path, "r") as data:
     for line in data:
-        info = line.split(",")
-        print(info)
+        info = line.strip().split(",")
+        name = info[0]
+        gwa = info[1]
+        print(f"Name: {name} | GWA: {gwa}")
